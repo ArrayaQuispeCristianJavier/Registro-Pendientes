548b0cfc227a0cae7061a51f5224589ea2851658
ArrayaQuispeCristianJavier <arrayacristian75@gmail.com>
2023-10-28 21:57:02 -0300

    Arreglo de errores en registro de tarea

--- src/components/ListaDeNotas.js
+++ src/components/ListaDeNotas.js
@@ -15,17 +15,21 @@ function ListaDeNotas() {
     //Agregar un pendiente
     const agregar = () => {
         //Si no se ingresa ninguna pendiente osea contiene algun texto
-        if (newNota !== "" && pendiente != newNota) {
+        //Devuelve verdadero si el elemento de newNotas esta en el arreglo de pendiente
+        //se le pone el "!" para indicar que si newNota no esta pendiente sera verdadero  pero en caso contrario  seria falso
+        //Se agrega includes para verificar si esta presente en el arreglo, es un booleano
+        if (newNota !== "" && !pendiente.includes(newNota)) {
             setPendiente([...pendiente, newNota]);//... es como hacer una copia en arrays            
             console.log(nota.estado);
             console.log("Se registro una tarea");//Verificacion del ingreso de una tarea pendiente
             console.log(pendiente);
         }
+        setNewNota("");//Limpia el input
     }
 
 
     //Funcion para eliminar un pendiente
-    function eliminar(notaToDelete) {
+    const eliminar = (notaToDelete)=> {
         setPendiente(pendiente.filter(nota => nota !== notaToDelete))
     }
 
